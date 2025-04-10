06f9b646587886995d02fea02b4ea68935651e5e
Piyush Pradhan <pradhanpiyush687@gmail.com>
2025-04-10 16:43:52 -0400

    Taylor Green vortex for Cascaded LBM

--- examples/multiphase/Cascaded/taylor_green_vortex.py
+++ examples/multiphase/Cascaded/taylor_green_vortex.py
@@ -1,5 +1,5 @@
 """
-Single component 2D Poiseuille flow driven by body force.
+Single component 2D Taylor Green vortex using Cascaded LBM.
 
 The collision matrix is based on:
 1. Fei, L. & Luo, K. H. Consistent forcing scheme in the cascaded lattice Boltzmann method. Phys. Rev. E 96, 053307 (2017).
@@ -21,26 +21,24 @@ from jax import vmap, jit
 from jax.tree import reduce, map
 
 
-class Poiseuille2D(MultiphaseCascade):
+class TaylorGreen2D(MultiphaseCascade):
     def initialize_macroscopic_fields(self):
         rho_tree = []
 
-        rho = np.ones((nx, ny, 1))
+        rho = np.zeros((self.nx, self.ny, 1))
+        rho[..., 0] = 3.0 * (1.0 - u_0**2 / 4.0 * (np.cos(2.0 * phi * x) + np.cos(2.0 * phi * y)))
         rho = self.distributed_array_init((self.nx, self.ny, 1), self.precisionPolicy.compute_dtype, init_val=rho)
         rho = self.precisionPolicy.cast_to_output(rho)
         rho_tree.append(rho)
 
         u = np.zeros((self.nx, self.ny, 2))
+        u[..., 0] = u_0 * np.sin(phi * x) * np.cos(phi * y)
+        u[..., 1] = -u_0 * np.cos(phi * x) * np.sin(phi * y)
         u = self.distributed_array_init((self.nx, self.ny, 2), self.precisionPolicy.compute_dtype, init_val=u)
         u = self.precisionPolicy.cast_to_output(u)
         u_tree = []
         u_tree.append(u)
         return rho_tree, u_tree
-
-    def set_boundary_conditions(self):
-        walls = np.concatenate((self.boundingBoxIndices["top"], self.boundingBoxIndices["bottom"]))
-        walls = tuple(walls.T)
-        self.BCs[0].append(BounceBack(walls, self.gridInfo, self.precisionPolicy, theta=theta[walls], phi=phi[walls], delta_rho=delta_rho[walls]))
 
     @partial(jit, static_argnums=(0,))
     def compute_potential(self, rho_tree):
@@ -68,6 +66,8 @@ class Poiseuille2D(MultiphaseCascade):
             "rho": rho[..., 0],
             "ux": u[..., 0],
             "uy": u[..., 1],
+            "u_analytical_y": u_analytical[..., 0],
+            "u_analytical_x": u_analytical[..., 1],
         }
         save_fields_vtk(
             timestep,
@@ -80,16 +80,6 @@ class Poiseuille2D(MultiphaseCascade):
 if __name__ == "__main__":
     e = LatticeD2Q9().c.T
     en = np.linalg.norm(e, axis=1)
-
-    # ex = np.array([0, 1, -1, 0, 0, 1, -1, 1, -1])
-    # ey = np.array([0, 0, 0, 1, -1, 1, -1, -1, 1])
-    # _e = np.zeros((9, 2))
-    # _e[:, 0] = ex
-    # _e[:, 1] = ey
-    # order = np.array([e.tolist().index((_e[i]).tolist()) for i in range(9)])
-    #
-    # lattice = LatticeD2Q9()
-    # lattice.e = _e.T
 
     # Cascaded LBM collision matrix
     M = np.array([
@@ -104,23 +94,35 @@ if __name__ == "__main__":
         [0, 0, 0, 0, 0, 1, 1, 1, 1],
     ])
 
-    nx = 100
-    ny = 50
+    N = 80
+    nx = N
+    ny = N
 
-    visc = 0.5
+    u_0 = 0.05
+    phi = 2 * np.pi / N
+
+    x = np.linspace(0, N - 1, N)
+    y = np.linspace(0, N - 1, N)
+    x, y = np.meshgrid(x, y)
+
+    Re = 150
+    visc = u_0 * np.pi / Re
+
+    F = np.zeros((nx, ny, 2))
+    F[..., 0] = 2 * visc * u_0 * phi**2 * np.cos(phi * x) * np.cos(phi * y)
+    F[..., 1] = 2 * visc * u_0 * phi**2 * np.sin(phi * x) * np.sin(phi * y)
+
+    u_analytical = np.zeros_like(F)
+    u_analytical[..., 0] = u_0 * np.sin(phi * x) * np.sin(phi * y)
+    u_analytical[..., 1] = u_0 * np.cos(phi * x) * np.cos(phi * y)
+
     s2 = 1 / (3 * visc + 0.5)
-    s2 = 1.754
-
-    s_0 = [s2]  # Mass conservation
-    s_1 = [s2]  # Fixed
+    s_0 = [1.0]  # Mass conservation
+    s_1 = [1.0]  # Fixed
     s_2 = [s2]
     s_b = [s2]
-    s_3 = [(16 - 8 * s2) / (8 - s2)]  # No slip
-    s_4 = [s2]  # No slip
-
-    theta = (np.pi / 2) * np.ones((nx, ny, 1))
-    phi = np.ones((nx, ny, 1))
-    delta_rho = np.zeros((nx, ny, 1))
+    s_3 = [1.0]  # [(16 - 8 * s2) / (8 - s2)]  # No slip
+    s_4 = [1.0]  # No slip
 
     precision = "f32/f32"
     kwargs = {
@@ -130,7 +132,7 @@ if __name__ == "__main__":
         "ny": ny,
         "nz": 0,
         "g_kkprime": np.zeros((1, 1)),
-        "body_force": [1e-6, 0.0],
+        "body_force": F,
         "omega": [s2],
         "k": [0.0],
         "A": np.zeros((1, 1)),
@@ -143,14 +145,14 @@ if __name__ == "__main__":
         "s_4": s_4,
         "sigma": [0.0],
         "precision": precision,
-        "io_rate": 1000,
+        "io_rate": 40000,
         "compute_MLUPS": False,
-        "print_info_rate": 1000,
+        "print_info_rate": 40000,
         "checkpoint_rate": -1,
         "checkpoint_dir": os.path.abspath("./checkpoints_"),
         "restore_checkpoint": False,
     }
 
     os.system("rm -rf output*/ *.vtk")
-    sim = Poiseuille2D(**kwargs)
-    sim.run(10000)
+    sim = TaylorGreen2D(**kwargs)
+    sim.run(800000)
